0bb52847767dc88b72a6c31ec836b4e6d3913867
Windi159 <107599426+Windi159@users.noreply.github.com>
2026-03-20 19:22:02 +0900

    2강 task 업데이트
    수업에서 배운 내용들만 사용하여 구현

--- 2강/task.R
+++ 2강/task.R
@@ -7,18 +7,18 @@ cylinder
 cylinders_ratio <- table(cylinder)
 cylinders_ratio # 빈도표
 
-cylinders_relative_ratio <- cylinders_ratio / length(cylinder)
-cylinders_relative_ratio # 상대빈도표
+prop.table(cylinders_ratio) # 상대빈도표
 
 # 예제 2-2
 barplot(cylinders_ratio, space=0) # barplot을 통해 막대그래프 그리기
 
 # 예제 2-3
-pie(cylinders_ratio, col=c("red", "yellow", "blue"))
+pie(cylinders_ratio, col=c("red", "yellow", "blue")) # pie를 통해 원형 그래프 그리기기
 
 # 예제 2-4
-percentage_of_cylinders_ratio <- cylinders_relative_ratio * 100
-percentage_of_cylinders_ratio
+prop.table(cylinders_ratio)
+# 상대 빈도표 == 요소들을 전부 더했을 때 값이 1이 되는 빈도표.
+# %로 표현하고 싶을 경우에는 100을 곱하면 된다.
 
 # 예제 3번 준비
 iris_data <- iris[1:50, 1]
@@ -34,22 +34,25 @@ median(iris_data) # 중앙값 == Q2
 min(iris_data) # 최솟값
 max(iris_data) # 최댓값
 iris_data_range <- max(iris_data) - min(iris_data) # 범위
+iris_data_range
 
 # Q1, Q2, Q3 구하기
-q_iris_data <- quantile(iris_data)
-q_iris_data
+Q1 <- quantile(iris_data, 1/4)
+Q1
 
-Q1 <- q_iris_data["25%"]
-Q2 <- q_iris_data["50%"]
-Q3 <- q_iris_data["75%"]
+Q2 <- quantile(iris_data, 2/4)
+Q2
+
+Q3 <- quantile(iris_data, 3/4)
+Q3
+
 IQR <- Q3 - Q1
+IQR
 
+# 예제 3-3
 boxplot(iris_data) # 상자도표 그리기
 hist(iris_data) # 히스토그램 그리기
 
-as.numeric(q_iris_data["100%"]) > as.numeric(Q3 + 1.5 * IQR)
-# 단순 값만 비교해야하기 때문에 as.numeric 사용
-# Q3 + 1.5 * IQ은 5.8 이고, 최대값도 5.8이지만, 5.8을 초과한 값이 outliner임
-as.numeric(q_iris_data["0%"]) < as.numeric(Q1 - 1.5 * IQR)
-
-boxplot.stats(iris_data)$out
+# 예제 3-4
+which(iris_data < Q1-1.5*IQR)
+which(iris_data > Q3+1.5*IQR)
